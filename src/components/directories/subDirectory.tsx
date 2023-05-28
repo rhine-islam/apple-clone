dadc83b7a08fb305291b08aa1478abaaab6faa17
Rhine <rhine.islam@cookietechltd.com>
2023-05-29 00:36:02 +0600

    ⚒️ Fix : directory dropdown added

--- src/components/directories/subDirectory.tsx
+++ src/components/directories/subDirectory.tsx
@@ -1,29 +1,48 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import data from "../../data/data";
 import style from "../../style/style";
 
 const SubDirectory = ({ title }: any | string) => {
-  // Object.entries(data.test).map((value: any) => {
-  //   value[0] === title ? console.log(value[1]) : null;
-  // });
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="px-12 py-4 md:px-1">
-      <h1 className={style.txtBlackSmB}>{title} </h1>
-      <FontAwesomeIcon
-        icon={faPlus}
-        className="absolute visible -mt-6 right-16 md:hidden"
-      />
-      <hr className="visible md:hidden" />
-
       <div className="">
+        <h1 className={style.txtBlackSmB}>{title} </h1>
+        {open === true ? (
+          <button
+            className="absolute visible -mt-4 right-16 md:hidden"
+            onClick={() => setOpen(false)}
+          >
+            <FontAwesomeIcon icon={faXmark} className="w-2 h-3" />
+          </button>
+        ) : (
+          <button
+            className="absolute visible -mt-4 right-16 md:hidden"
+            onClick={() => setOpen(true)}
+          >
+            <FontAwesomeIcon icon={faPlus} className="w-2 h-3" />
+          </button>
+        )}
+
+        <hr className="visible mt-3 md:hidden" />
+      </div>
+
+      <div
+        className={`${
+          open
+            ? "visible space-y-4 ml-4 animate-flip-down animate-ease-out mt-3"
+            : "hidden"
+        } md:block`}
+      >
         {Object.entries(data.directories).map((key: any, i: number) =>
           key[0] === title
             ? key[1].map((value: any, id: number) => {
                 return (
                   <p
-                    className="hidden text-sm text-gray-700 text-start md:block font-sf"
+                    className="text-sm text-gray-700 text-start font-sf"
                     key={id}
                   >
                     {value}
@@ -33,16 +52,6 @@ const SubDirectory = ({ title }: any | string) => {
             : null
         )}
       </div>
-      {/* {data.subDirectories.map((link: string, id: number) => {
-        return (
-              <p
-                className="hidden text-sm text-gray-700 text-start md:block"
-                key={id}
-              >
-                {value[1]}
-              </p>
-            );
-      })} */}
     </div>
   );
 };
