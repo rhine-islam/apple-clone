ce4901b8d751f75d6139b10fb5ffceda8eaecfe3
Rhine <rhine.islam@cookietechltd.com>
2023-05-28 10:50:37 +0600

    ⚒️ Fix : Footer fix

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,18 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mb-8 px-12 md:px-2">
+    <div className="px-12 mb-8 md:px-2">
       <hr />
-      <div className="flex items-center flex-col ">
+      <div className="grid items-center grid-rows-2 md:flex md:justify-around">
         <p className="text-xs text-gray-500">
           Copyright © 2023 Apple Inc. All rights reserved.
         </p>
-        <div className="grid items-center justify-center grid-cols-4 space-x-4 divide-x divide-gray-400">
-          <p className="text-xs text-center text-black"> Privacy Policy</p>
-          <p className="text-xs text-center text-black"> Terms of Use </p>
-          <p className="text-xs text-center text-black"> Sales and Refunds </p>
-          <p className="text-xs text-center text-black"> Legal Site Map</p>
+        <div className="grid grid-cols-4 py-1 -ml-4 text-center divide-x divide-gray-400 md:space-x-4 md:mx-28">
+          <p className="text-xs text-black "> Privacy Policy</p>
+          <p className="text-xs text-black"> Terms of Use </p>
+          <p className="text-xs text-black"> Sales and Refunds </p>
+          <p className="text-xs text-black"> Legal Site Map</p>
         </div>
+        <p className="text-xs text-gray-900">United State</p>
       </div>
     </div>
   );
